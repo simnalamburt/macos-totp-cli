d3b59428238d087c0d9c4d876730db37ed3771ee
Hyeon Kim <simnalamburt@gmail.com>
2021-10-15 08:16:44 +0900

    Parse QR code image

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/simnalamburt/macos-totp-cli
 
 go 1.17
 
-require github.com/spf13/cobra v1.2.1
+require (
+	github.com/makiuchi-d/gozxing v0.1.1
+	github.com/spf13/cobra v1.2.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"net/url"
+	"os"
 
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/qrcode"
 	"github.com/spf13/cobra"
 )
 
@@ -10,15 +19,54 @@ func main() {
 	var cmdScan = &cobra.Command{
 		Use:   "scan [image file]",
 		Short: "Scan a QR code image",
-		Long: `Scan a QR code image and print its contents to the stdout.`,
-		Args: cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			// TODO
-			fmt.Println("Print: " + args[0])
+		Long:  `Scan a QR code image and store it to the macOS keychain.`,
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// open and decode image file
+			path := args[0]
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			img, _, err := image.Decode(file)
+			if err != nil {
+				return err
+			}
+
+			// prepare BinaryBitmap
+			bmp, err := gozxing.NewBinaryBitmapFromImage(img)
+			if err != nil {
+				return err
+			}
+
+			// decode image
+			qrReader := qrcode.NewQRCodeReader()
+			result, err := qrReader.Decode(bmp, nil)
+			if err != nil {
+				return err
+			}
+
+			// parse TOTP URL
+			parsed, err := url.Parse(result.GetText())
+			if err != nil {
+				return err
+			}
+			secret := parsed.Query().Get("secret")
+			if parsed.Scheme != "otpauth" || parsed.Host != "totp" || secret == "" {
+				return errors.New("Given QR code is not for TOTP")
+			}
+
+			// TODO: Store it to the keychain
+			fmt.Println(secret)
+
+			return nil
 		},
 	}
 
 	var rootCmd = &cobra.Command{Use: "totp"}
 	rootCmd.AddCommand(cmdScan)
-	rootCmd.Execute()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
